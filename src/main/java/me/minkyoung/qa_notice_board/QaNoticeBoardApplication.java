4b34ed16978a5abce50c238a3baa9ea08e840b91
MINKYOUNG\kyoun <vldzmehowl32@naver.com>
2025-05-19 21:40:56 +0900

    first commit

---
+++ src/main/java/me/minkyoung/qa_notice_board/QaNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package me.minkyoung.qa_notice_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaNoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaNoticeBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/minkyoung/qa_notice_board/QaNoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minkyoung.qa_notice_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaNoticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
